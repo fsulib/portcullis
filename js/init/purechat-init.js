1c28aade82c4c2e10708fcfcc41e5f8603ac70e0
Favenzio Calvo <fcalvo@fsu.edu>
2020-01-29 18:31:03 -0500

    Ready for initial testing.

--- js/init/purechat-init.js
+++
@@ -1,9 +0,0 @@
-(function ($, Drupal, drupalSettings) {
-  Drupal.behaviors.mtPurechat = {
-    attach: function (context, settings) {
-      $(context).find('body').once('mtPurechatInit').each(function() {
-        window.purechatApi = { l: [], t: [], on: function () { this.l.push(arguments); } }; (function () { var done = false; var script = document.createElement('script'); script.async = true; script.type = 'text/javascript'; script.src = 'https://app.purechat.com/VisitorWidget/WidgetScript'; document.getElementsByTagName('HEAD').item(0).appendChild(script); script.onreadystatechange = script.onload = function (e) { if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) { var w = new PCWidget({c: drupalSettings.catalogplus.purechatInit.purechatKey, f: true }); done = true; } }; })();
-      });
-    }
-  };
-})(jQuery, Drupal, drupalSettings);
