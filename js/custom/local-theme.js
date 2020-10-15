97da082e091f9150ac0725272feba10bd9575392
Favenzio Calvo <fcalvo@admin.fsu.edu>
2020-10-15 15:29:14 -0400

    Changed anchor text for Advanced Search.

--- js/custom/local-theme.js
+++ js/custom/local-theme.js
@@ -1,14 +1,14 @@
 jQuery(document).ready(function($) {
-  $("#display-advanced-search").click(function() {
+  $(".display-advanced-search").click(function() {
     $(".form-item-search-api-fulltext-1").toggle();
     $(".form-item-search-api-fulltext-2").toggle();
     
-    var searchType = $("#display-advanced-search").text();
+    var searchType = $(".display-advanced-search").text();
     
-    if(searchType === 'Advanced Search') {
-      $("#display-advanced-search").text('Simple Search');
+    if(searchType === 'Go to Advanced Search') {
+      $(".display-advanced-search").text('Go to Simple Search');
     } else {
-      $("#display-advanced-search").text('Advanced Search');
+      $(".display-advanced-search").text('Go to Advanced Search');
     }
   });
 });
