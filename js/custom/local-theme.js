3cfd0e0a2a7c133d6362b8bc280244fcc3a4682b
Brian Arsenault <barsenault@fsu.edu>
2022-09-21 17:13:58 -0400

    fixes js bug on advanced search

--- js/custom/local-theme.js
+++ js/custom/local-theme.js
@@ -1,24 +1,32 @@
-jQuery(document).ready(function($) {
-  $(".display-advanced-search").click(function() {
-    $(".form-item-search-api-fulltext-1").toggle();
-    $(".form-item-search-api-fulltext-2").toggle();
-    
-    var searchType = $(".display-advanced-search").text();
-    
-    if(searchType === 'Go to Advanced Search') {
-      $(".display-advanced-search").text('Go to Simple Search');
-    } else {
-      $(".display-advanced-search").text('Go to Advanced Search');
-    }
-  });
+(function ($, Drupal, drupalSettings) {
 
-  if($("input[name=search_api_fulltext_1]").val() || $("input[name=search_api_fulltext_2]").val()) {
-    $(".form-item-search-api-fulltext-1").show();
-    $(".form-item-search-api-fulltext-2").show();
+  Drupal.behaviors.portcullis = {
+    attach: function (context, settings) {
+
+      $(context).find(".display-advanced-search").on().click(function () {
+        $(".form-item-search-api-fulltext-1").toggle();
+        $(".form-item-search-api-fulltext-2").toggle();
+
+        var searchType = $(".display-advanced-search").text();
+
+        if (searchType === 'Go to Advanced Search') {
+          $(".display-advanced-search").text('Go to Simple Search');
+        } else {
+          $(".display-advanced-search").text('Go to Advanced Search');
+        }
+      });
+
+      if ($("input[name=search_api_fulltext_1]").val() || $("input[name=search_api_fulltext_2]").val()) {
+        $(".form-item-search-api-fulltext-1").show();
+        $(".form-item-search-api-fulltext-2").show();
+        $(".display-advanced-search").css('display','none');
+      }
+
+    }
   }
 
   $(".deactivate-link").removeAttr("href");
-  
+
   // Change http to https in production
   var ldbase_download_url = $(".ldbase-file-download").attr('href');
   const parsed_url = new URL(ldbase_download_url);
@@ -27,4 +35,4 @@ jQuery(document).ready(function($) {
     $(".ldbase-file-download").attr('href', parsed_url.href);
   }
 
-});
+})(jQuery, Drupal, drupalSettings);
